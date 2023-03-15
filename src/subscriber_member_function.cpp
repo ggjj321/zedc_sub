7f59a8be983f3eb4cd3b0513db6418ee36038467
ggjj321 <10370324lsjh@gmail.com>
2023-03-15 18:03:38 +0800

    build od data publisher

--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -17,8 +17,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include <zed_interfaces/msg/objects_stamped.hpp>
+#include "std_msgs/msg/string.hpp"
 
 using std::placeholders::_1;
+
+using namespace std::chrono_literals;
 
 class MinimalSubscriber : public rclcpp::Node
 {
@@ -26,6 +29,8 @@ public:
   MinimalSubscriber()
   : Node("minimal_subscriber")
   {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("transfrom_zed2_3dod_to_2d", 1000);
+    
     subscription_ = this->create_subscription<zed_interfaces::msg::ObjectsStamped>(
       "/zed2/zed_node/obj_det/objects", 1000, std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
@@ -37,10 +42,36 @@ private:
       float x = obj.position[0];
       float y = obj.position[1];
       float z = obj.position[2];
-      RCLCPP_INFO(this->get_logger(), "detect position: '%f' '%f' '%f' " + obj.label, x, y, z);
+
+      int bx = projectTo2d_x(x, y, z);
+      int by = projectTo2d_y(x, y, z);
+
+      auto message = std_msgs::msg::String();
+      message.data = "2d: " + std::to_string(bx) + " " + std::to_string(by) + " ";
+      message.data += "label: " + obj.label + " ";
+      message.data += "label_id: " + std::to_string(obj.label_id) + " ";
+      RCLCPP_INFO(this->get_logger(), message.data);
+      publisher_->publish(message);
     }
   }
+
+  int projectTo2d_x(double x, double y, double z)  
+  {
+      double ax = x;  
+      double eod = z;  
+      double result = ax * (10 / eod);  
+      return round(result);  
+  }
+
+  int projectTo2d_y(double x, double y, double z)  
+  {
+      double ay = y;
+      double eod = z;
+      double result = ay * (10 / eod);
+      return round(result);
+  }
   rclcpp::Subscription<zed_interfaces::msg::ObjectsStamped>::SharedPtr subscription_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
 };
 
 int main(int argc, char * argv[])
